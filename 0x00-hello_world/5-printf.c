9985bab62abdbc68a843d7489c314c1684dc3fa3
root <Dorcasadams774@gmail.com>
2022-10-17 08:03:48 -0700

    Made a few corrections

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- *main- This is the enty point
+ *main- This is the entry point
  *
  *Return: If there are no errors it returns zero
  */
 int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art,");
+	printf("with proper grammer, but the outcome is a piece of art,\n");
 	return (0);
 }
