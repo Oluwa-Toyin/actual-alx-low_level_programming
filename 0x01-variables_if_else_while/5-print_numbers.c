eca58d1f2e5d0816bb6f3aa4a4cd58e29d2e6f8e
root <Dorcasadams774@gmail.com>
2022-10-17 21:21:55 -0700

    Last update this morning

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main- This is the program's entry point.
+ *Return: If there is no error, the return is zero
+ **/
+
+int main(void)
+
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
+
+
